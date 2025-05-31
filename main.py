0c1dbcb89a1d8ea591ce650696cbab3f102829d1
deginmulbah <jenkinsmulbah6@gmail.com>
2025-05-31 11:58:29 +0000

    read file content

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path, "r") as file: 
+        file_content = file.read()
+    return file_content
+
+def main():
+    file_content = get_book_text("./books/frankenstein.txt")
+    print(file_content)
+    
+main()
